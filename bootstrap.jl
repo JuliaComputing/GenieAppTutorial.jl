6e15354c8c41a0b64ec1e8143ab7ff422ff59e9c
jeremie.db <jeremie.db@evovest.com>
2021-12-21 10:12:55 -0500

    up

--- bootstrap.jl
+++ bootstrap.jl
@@ -1,5 +1,5 @@
 (pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
 
-using GenieApp
-push!(Base.modules_warned_for, Base.PkgId(GenieApp))
+using GenieAppTutorial
+push!(Base.modules_warned_for, Base.PkgId(GenieAppTutorial))
 GenieApp.main()
--- src/GenieApp.jl
+++ src/GenieApp.jl
@@ -1,4 +1,4 @@
-module GenieApp
+module GenieAppTutorial
 
 using Genie, Logging, LoggingExtras
 
