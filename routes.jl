2e465382cefd310f037bf849faf6de90be1f1fb1
jeremie.db <jeremie.db@evovest.com>
2021-12-21 11:08:23 -0500

    up

--- routes.jl
+++ routes.jl
@@ -1,6 +1,12 @@
-using Genie.Router
-using Genie.Renderer.Html
+using Genie, Stipple, StippleUI, StipplePlotly
+using Genie.Router, Genie.Renderer.Html
 using DashboardsController
+
+if isprod()
+  for m in [Genie, Stipple, StippleUI, StipplePlotly]
+    m.assets_config.host = "https://cdn.statically.io/gh/GenieFramework"
+  end
+end
 
 route("/") do
   html(:dashboards, "dashboards.jl"; layout=:app, context = DashboardsController, DashboardsController.render()...)
