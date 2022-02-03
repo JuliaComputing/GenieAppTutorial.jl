62809d4c8ce40ec420283106029ded6e24510676
jeremie.db <jeremie.db@evovest.com>
2022-02-02 22:03:04 -0500

    up

--- bin/main.jl
+++ bin/main.jl
@@ -1,5 +1,9 @@
 push!(LOAD_PATH, abspath(normpath(joinpath(@__DIR__, "../src/"))))
 
+@info "@__DIR__: " @__DIR__
+@info "pwd(): " pwd()
+@info "LOAD_PATH: " LOAD_PATH
+
 ENV["STARTSERVER"] = true
 ENV["GENIE_ENV"] = "prod"
 ENV["EARLYBIND"] = true
