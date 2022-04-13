b9a9a4fd077950a533afaa2c80ff6187c6924900
Jeremie Bouchard <jeremie.bouchard@juliacomputing.com>
2022-04-13 15:34:56 -0400

    up

--- bin/main.jl
+++ bin/main.jl
@@ -1,14 +1,14 @@
 @info "@__DIR__: " @__DIR__
 @info "pwd(): " pwd()
 
-ENV["HOST"] = "127.0.0.1"
+# ENV["HOST"] = "127.0.0.1"
 
 push!(LOAD_PATH, abspath(normpath(joinpath(@__DIR__, "../src/"))))
 @info "LOAD_PATH: " LOAD_PATH
 
 ENV["STARTSERVER"] = true
 ENV["GENIE_ENV"] = "prod"
-ENV["EARLYBIND"] = true
+ENV["EARLYBIND"] = false
 
 
 @info "haskey EARLYBIND" haskey(ENV, "EARLYBIND")
