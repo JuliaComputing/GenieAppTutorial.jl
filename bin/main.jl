358c9444f441bd07471868ebd58060d91690da4f
jeremie.bouchard <jeremie.bouchard@juliacomputing.com>
2022-04-29 16:13:55 -0400

    up

--- bin/main.jl
+++ bin/main.jl
@@ -3,15 +3,19 @@
 
 ENV["HOST"] = "0.0.0.0"
 
-push!(LOAD_PATH, abspath(normpath(joinpath(@__DIR__, "../src/"))))
+push!(LOAD_PATH, abspath(realpath(joinpath(@__DIR__, "../src/"))))
+push!(LOAD_PATH, abspath(realpath(joinpath(@__DIR__, ".."))))
 @info "LOAD_PATH: " LOAD_PATH
 
 ENV["STARTSERVER"] = true
 ENV["GENIE_ENV"] = "prod"
 ENV["EARLYBIND"] = true
 
-
 @info "haskey EARLYBIND" haskey(ENV, "EARLYBIND")
 @info "EARLYBIND" ENV["EARLYBIND"]
 
-include("../bootstrap.jl")
+# include("../bootstrap.jl")
+a = abspath(realpath(joinpath(@__DIR__, "..")))
+@info a 
+cd(a)
+run(`julia --project=. bootstrap.jl`)
