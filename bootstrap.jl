c474f07e58285ed849ab2936fd8fd2dc8ef0fc4a
Matt Bauman <mbauman@juliacomputing.com>
2022-08-11 20:45:10 +0000

    add a change

--- bootstrap.jl
+++ bootstrap.jl
@@ -1,5 +1,6 @@
 (pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
 
+# Add some comment...
 using GenieAppTutorial
 push!(Base.modules_warned_for, Base.PkgId(GenieAppTutorial))
 GenieAppTutorial.main()
